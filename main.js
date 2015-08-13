0ffb0c5c9fd771633f6ec78fa7e543b09e7be7fe
Thomas Tortorini <thomastortorini@gmail.com>
2015-08-13 17:25:11 +0200

    use of jQuery

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var
-	elHtmlDiv,
-	demoCode,
+	jqHtml,
+	jqDemoCode,
 	orphanTags = {
 		"INPUT": true,
 		"IMG": true,
@@ -23,21 +23,20 @@ var
 function writeNode( node, open ) {
 	var
 		i, o, v,
-		elSpan = null,
+		jqSpan = null,
 		type = node.nodeType,
 		tag = node.nodeName,
 		isOrphan = orphanTags[ tag ],
-		spanClss = "",
 		spanHtml = ""
 	;
 	if ( ( type === 1 && !isOrphan ) || open ) {
-		elSpan = document.createElement( "span" );
+		jqSpan = $( "<span>" );
 		switch ( type ) {
 			case Node.ELEMENT_NODE:
-				spanClss = "node " + ( !open
-					? "tB"
-					: ( isOrphan ? "tA tB" : "tA" )
-				);
+				jqSpan.addClass( "node " + ( !open ?
+					"tB" :
+					isOrphan ? "tA tB" : "tA"
+				));
 				spanHtml = "<span class='tag'>" + tag.toLowerCase() + "</span>";
 				if ( open ) {
 					for ( i = 0; o = attrs[ i ]; ++i ) {
@@ -49,7 +48,7 @@ function writeNode( node, open ) {
 				}
 			break;
 			case Node.TEXT_NODE:
-				spanClss = "node text";
+				jqSpan.addClass( "node text" );
 				for ( i = 0; o = node.data[ i ]; ++i ) {
 					switch ( o ) {
 						default   : spanHtml += o; break;
@@ -59,65 +58,71 @@ function writeNode( node, open ) {
 				}
 			break;
 			case Node.COMMENT_NODE:
-				spanClss = "node comment";
+				jqSpan.addClass( "node comment" );
 				spanHtml = "&lt;!-- "+ node.data +" --&gt;";
 			break;
 		}
-		elSpan.className = spanClss;
-		elSpan.innerHTML = spanHtml;
-		elHtmlDiv.appendChild( elSpan );
+		jqSpan.html( spanHtml );
+		jqHtml.append( jqSpan );
+		return jqSpan;
 	}
-	return elSpan;
+	return null;
 }
 
-function list(node) {
-	for (; node !== null; node = node.nextSibling) {
+function list( node ) {
+	for ( ; node !== null; node = node.nextSibling ) {
 		node._span = [];
-		node._span[0] = writeNode(node, 1);
-		list(node.firstChild);
-		node._span[1] = writeNode(node, 0);
+		node._span[ 0 ] = writeNode( node, 1 );
+		list( node.firstChild );
+		node._span[ 1 ] = writeNode( node, 0 );
 	}
 }
 
-function selectDOM(selector) {
+function selectDOM( selector ) {
 	var selected = !!selector * 1;
-	if (selector)
-		selectDOM.elems = demoCode.querySelectorAll(selector);
-	if (selectDOM.elems) {
-		elHtmlDiv.setAttribute('selected', selected);
-		for (var i = 0, e; e = selectDOM.elems[i]; ++i) {
-			if (e._span[0]) e._span[0].setAttribute('selected', selected);
-			if (e._span[1]) e._span[1].setAttribute('selected', selected);
-		}
-		if (!selector)
+	if ( selector ) {
+		selectDOM.elems = jqDemoCode.find( selector );
+	}
+	if ( selectDOM.elems && selectDOM.elems.length ) {
+		jqHtml.attr( "select", selected );
+		selectDOM.elems.each( function() {
+			if ( this._span[ 0 ] ) { this._span[ 0 ].attr( "select", selected ); }
+			if ( this._span[ 1 ] ) { this._span[ 1 ].attr( "select", selected ); }
+		});
+		if ( !selector ) {
 			selectDOM.elems = null;
+		}
 	}
 }
 
 window.onload = function() {
-	elHtmlDiv  = document.getElementById('html');
-	demoCode = document.getElementById('demoCode');
-	var helpDiv = document.getElementById('help');
-	list(demoCode.firstChild);
-	var links = document.getElementById('panel').getElementsByTagName('a');
-	var linkSelected = null;
-	for (var i = 0, a; a = links[i]; ++i) {
-		a._helpStr = a.title;
-		a.title = "";
-		a.onclick = function() {
+	jqHtml = $( "#html" );
+	jqDemoCode = $( "#demoCode" );
+	list( jqDemoCode.children()[ 0 ] );
+	var
+		jqHelpDiv = $( "#help" ),
+		elLinkSelected = null
+	;
+
+	$( "#panel a" ).each( function() {
+		this._helpStr = this.title;
+		this.title = "";
+		$( this ).click( function() {
 			selectDOM();
-			if (linkSelected)
-				linkSelected.className = "";
-			if (linkSelected === this) {
-				linkSelected = null;
-				helpDiv.innerHTML = "";
+			if ( elLinkSelected ) {
+				elLinkSelected.className = "";
+			}
+			if ( elLinkSelected === this ) {
+				jqHelpDiv.text( "" );
+				elLinkSelected = null;
 			} else {
-				this.className = 'selected';
-				helpDiv.innerHTML = this._helpStr;
-				selectDOM(this.innerHTML.replace(/&gt;/g, '>'));
-				linkSelected = this;
+				this.className = "select";
+				jqHelpDiv.text( this._helpStr );
+				selectDOM( this.innerHTML.replace( /&gt;/g, ">" ) );
+				elLinkSelected = this;
 			}
 			return false;
-		};
-	}
+		});
+
+	});
 };
